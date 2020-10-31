1456c46f26f7e1a01ea34765ca485eebfdf13832
Tomara Petty <tomararuth@gmail.com>
2020-10-31 08:30:47 -0700

    added pseudo code.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Start button - add event listener 
+// Score keeper - activity 11 increment/decrement & activity 18 shopping cart 
+// Prompts for questions to pop up
+// Timer starts and activates first question activity 8 - timer + 29 timer app 
+// End quiz when all questions are answered or timer reaches 0 - should go through TODO 25 & 26
+// Save initials and score - potentially activity 22 for local storage signUpButton and recall info.
+
+
+
